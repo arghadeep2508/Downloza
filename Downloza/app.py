f9fcb70f2f76259d4234b9fc1fe62aa083a16e82
Arghadeep Dutta <arghadeepdutta2025@gmail.com>
2026-03-05 12:13:30 +0530

    Update app.py

--- Downloza/app.py
+++ Downloza/app.py
@@ -1,5 +1,13 @@
-from flask import send_from_directory
+from flask import Flask, render_template, request, jsonify, send_file, send_from_directory
 
+app = Flask(__name__)
+
+# existing routes
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+# Monetag verification/service worker
 @app.route('/sw.js')
 def monetag_sw():
     return send_from_directory('static', 'sw.js')
